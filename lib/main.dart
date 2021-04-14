bae5009c5e498c919e261276b4f7d971c2e9fc71
dezen <dezen21@gmail.com>
2021-04-14 11:26:42 +0700

    belajar

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:chaty/pages/homepage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: 'Poppins'),
+      home: HomePage(),
+    );
+  }
+}
---
+++ lib/widgets/chat_tile.dart
@@ -0,0 +1,50 @@
+import 'package:chaty/pages/theme.dart';
+import 'package:flutter/material.dart';
+
+class ChatTile extends StatelessWidget {
+  final String imageUrl;
+  final String name;
+  final String text;
+  final String time;
+  final bool unread;
+
+  ChatTile({this.imageUrl, this.name, this.text, this.time, this.unread});
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(top: 16),
+      child: Row(
+        children: [
+          Image.asset(
+            imageUrl,
+            height: 55,
+            width: 55,
+          ),
+          SizedBox(
+            width: 16,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                name,
+                style: titleTextStyle,
+              ),
+              Text(
+                text,
+                style: unread
+                    ? subtitleTextStyle.copyWith(color: blackColor)
+                    : subtitleTextStyle,
+              )
+            ],
+          ),
+          Spacer(),
+          Text(
+            time,
+            style: subtitleTextStyle,
+          )
+        ],
+      ),
+    );
+  }
+}
